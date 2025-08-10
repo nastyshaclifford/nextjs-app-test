baf013d167e23aa4c2c764300be06e9074b69e4d
nastyshaclifford <alieksankova2002@mail.ru>
2025-08-10 16:17:27 +0300

    fix/bag page.tsx

--- src/app/userpage/[id]/page.tsx
+++ src/app/userpage/[id]/page.tsx
@@ -1,7 +1,7 @@
-    import React from "react";
     import UserPageClient from "./UserPageClient";
+    import { User } from "@/types/user";
 
-    async function getUser(id: number) {
+    async function getUser(id: number): Promise<User | null> {
     const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
         next: { revalidate: 60 },
     });
@@ -9,21 +9,18 @@
     return res.json();
     }
 
-    interface UserPageProps {
-    params: {
-        id: string;
-    };
+    interface PageProps {
+    params: { id: string };
     }
 
-    export default async function UserPage({ params }: UserPageProps) {
+    export default async function UserPage({ params }: PageProps) {
     const user = await getUser(Number(params.id));
 
-    if (!user) {
-        return <div>Ошибка загрузки пользователя</div>;
-    }
+    if (!user) return <div>Пользователь не найден</div>;
 
     return <UserPageClient initialUser={user} />;
     }
+
 
 
 
